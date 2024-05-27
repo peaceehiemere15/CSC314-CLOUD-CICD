6da83b04c4687cdba6fd144834a1ceb93d48d14c
peaceehiemere15 <peaceehiemere15@gmail.com>
2024-05-27 17:47:11 +0100

    third commit

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// app.use(express.static('public'))
-app.get('/', (req, res) => res.send(
-' this is cCSC314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME: PEACE EHIEMERE MATRIC NUMBER: VUG/CSC/22/7851'
-));
+app.use(express.static('public'))
+// app.get('/', (req, res) => res.send(
+// ' this is cCSC314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME: PEACE EHIEMERE MATRIC NUMBER: VUG/CSC/22/7851'
+// ));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
