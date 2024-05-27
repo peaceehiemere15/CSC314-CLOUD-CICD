6626804661f5406e187aa67754e9d1bfb5fa672a
daniel osagie <danielosagie341@gmail.com>
2024-05-27 13:10:05 +0100

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE STEP 1 !'));
+app.get('/', (req, res) => res.send(
+    ' this is cCSC314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME: OSAGIE DANIEL EHIREHMEN MATRIC NUMBER: VUG/CSC/22/7545'
+));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
